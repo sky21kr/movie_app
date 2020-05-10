f8a76a27facada4fb9b5d7380e50b26a275f55e0
ShinSeungMin <sky2197kr@gmail.com>
2020-05-10 20:38:11 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ id, name, score, rating }) {
-  return (
-    <span>
-      <h1>I like {name} </h1>
-      <h2>{score}</h2>
-      <h2>{rating}</h2>
-    </span>
-  );
-}
-
-const foodILike = [
-  {
-    id: 1,
-    name: "bibimbap",
-    score: 10,
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "kimbap",
-    score: 6,
-    rating: 4.8,
-  },
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  score: PropTypes.number.isRequired,
-  rating: PropTypes.number,
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food name={dish.name} score={dish.score} rating={dish.rating} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+      setTimeout(() => {
+      this.setState({ isLoading: false});
+      }, 6000);
+  }
+  render() {
+      const { isLoading } = this.state;
+    return <div>{this.state.isLoading ? "Loding" : "We are ready"}</div>;
+  }
 }
 
 export default App;
