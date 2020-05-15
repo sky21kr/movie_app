8a43ce0a848f1150e8ea45a194eb327d18ab1b7a
ShinSeungMin <sky2197kr@gmail.com>
2020-05-15 22:30:47 +0900

    Updates

---
+++ precache-manifest.010ee7c97177bf6a41ac7838d6225420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52969f6d1db9d38586e2f846bfdcccb5",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7cfe688d14806f463769",
+    "url": "/movie_app/static/css/main.6ccf5d5a.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfe688d14806f463769",
+    "url": "/movie_app/static/js/main.10a9a171.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
